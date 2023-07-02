0a67caf4fbc5291de5c92ebe19b6832d8e6c5332
LEEBONGHAK <dlqhdgkr456@gmail.com>
2023-07-02 21:34:08 +0900

    6.12 ApiService

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
@@ -19,6 +21,34 @@ class ApiService {
       for (var webtoon in webtoons) {
         final toon = WebtoonModel.fromJson(webtoon);
         webtoonInstances.add(toon);
+      }
+      return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getWebtoonById(String id) async {
+    final url = Uri.parse('$baseUrl/$id');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisode>> getWebtoonEpisodesById(String id) async {
+    List<WebtoonEpisodeModel> webtoonInstances = [];
+    final url = Uri.parse('$baseUrl/$id/episodes');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+
+      for (var episode in episodes) {
+        webtoonInstances.add(WebtoonEpisodeModel.fromJson(episode));
       }
       return webtoonInstances;
     }
